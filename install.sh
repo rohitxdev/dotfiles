a7ea858f1ca161fceee367f91341a9ae7418f0ab
Rohit Reddy <rohitreddy.gangwar@gmail.com>
2026-03-14 19:39:39 +0000

    fix: eza install

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc \
   | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" \
   | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo apt update
 sudo apt install -y eza
 
 # --- zoxide ---
